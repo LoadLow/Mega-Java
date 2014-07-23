4b926d4431804b66765a6bfff6d45afeea7a2bc0
LoadLow <LoadLow@users.noreply.github.com>
2014-07-23 16:29:23 +0200

    Change project head description
    Adds my fork

--- src/nz/co/mega/api/MegaFile.java
+++ src/nz/co/mega/api/MegaFile.java
@@ -4,10 +4,14 @@
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- * 
+ *
  * Contributors:
  *     @NT2005 - initial API and implementation
+ * Fork:
+ *     LoadLow - SecureRandom and anonymous login etc..
  ******************************************************************************/
+package nz.co.mega.api;
+
 public class MegaFile {
 
     private String uid, name, h;
